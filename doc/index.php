e864c866936fefd0c6afc7fe60deb2bc5675204a
DevTeam <dev@oodkjs.org>
2016-08-04 11:03:21 +0200

    update doc

--- doc/index.php
+++ doc/index.php
@@ -14,12 +14,12 @@
     <script type="text/javascript" src="lib/highlight/highlight.pack.js"></script>
     
     <!-- Latest compiled and minified JavaScript -->
-    <script src="lib/bootstrap/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap-theme.min.css">
   
     <link rel="stylesheet" href="lib/jquery-ui/jquery-ui.min.css" type="text/css"/>
 
